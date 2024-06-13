474f3c3dde9c0aca8bd435ccd12b7d256c1d0449
lilalr <gurennut@gmail.com>
2024-06-13 18:23:23 +0700

    constructor

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -6,4 +6,17 @@ using namespace std;
 class Node
 {
 public:
-	
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	// constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+
+		
+};
